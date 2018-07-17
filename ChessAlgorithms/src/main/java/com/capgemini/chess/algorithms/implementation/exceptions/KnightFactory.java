3324b5dc0c7ca5026a5764432e647a9311347f9d
PBOCER <przemek.bocer@gmail.com>
2018-07-17 12:08:30 +0200

    Add knight validation

--- ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/exceptions/KnightFactory.java
+++ ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/exceptions/KnightFactory.java
@@ -1,48 +1,42 @@
 package com.capgemini.chess.algorithms.implementation.exceptions;
 
 import com.capgemini.chess.algorithms.data.Coordinate;
-import com.capgemini.chess.algorithms.data.enums.Color;
 import com.capgemini.chess.algorithms.data.enums.MoveType;
 import com.capgemini.chess.algorithms.data.enums.Piece;
 import com.capgemini.chess.algorithms.data.enums.PieceType;
 
-public class KingFactory implements Factory {
+public class KnightFactory implements Factory {
+
+
     @Override
     public boolean moveValidation(Piece piece, Coordinate fromPlace, Coordinate toPlace, MoveType moveType) throws InvalidMoveException {
 
         PieceType pieceType = piece.getType();
 
-
         int changeOfLocationX = Math.abs(fromPlace.getX() - toPlace.getX());
         int changeOfLocationY = Math.abs(fromPlace.getY() - toPlace.getY());
 
+
+        //TODO upewnij sie mcz tyle metod i czy dobre zakresy
+
         switch (pieceType) {
-
-            case KING:
-                if (changeOfLocationX == 1 && changeOfLocationY == 0) {
+            case KNIGHT:
+                if (changeOfLocationX == 1 && changeOfLocationY == 2) {
                     return true;
-
-                } else if (changeOfLocationX == 0 && changeOfLocationY == 1) {
+                } else if (changeOfLocationX == 2 && changeOfLocationY == 1) {
                     return true;
-                } else if (changeOfLocationX == 1 && changeOfLocationY == 0) {
-                    return true;
-                } else if (changeOfLocationX == 1 && changeOfLocationY == 1) {
-                    return true;
-                } else throw new
-                        InvalidMoveException();
+                } else {
+                    throw new InvalidMoveException();
+                }
         }
 
 
 
-                throw new InvalidMoveException();
-        }
+        throw new InvalidMoveException();
+    }
 
 
 
 
 
 }
-
-
-
-
