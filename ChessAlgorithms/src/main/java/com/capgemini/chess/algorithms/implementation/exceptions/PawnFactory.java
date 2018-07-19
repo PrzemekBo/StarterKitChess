4d2f0dacc581874ad6465cbdfcfb582cf5bdb9f9
PBOCER <przemek.bocer@gmail.com>
2018-07-19 08:20:10 +0200

    Update Pawn

--- ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/exceptions/PawnFactory.java
+++ ChessAlgorithms/src/main/java/com/capgemini/chess/algorithms/implementation/exceptions/PawnFactory.java
@@ -7,45 +7,98 @@ import com.capgemini.chess.algorithms.data.enums.Piece;
 import com.capgemini.chess.algorithms.data.generated.Board;
 
 
-public class PawnFactory   {
+public class PawnFactory extends GameValidator  {
 
     public boolean moveValidation(Piece piece, Coordinate fromPlace, Coordinate toPlace, MoveType moveType) {
 
-
         int changeOfLocationX = Math.abs(fromPlace.getX() - toPlace.getX());
-        int changeOfLocationY = Math.abs(fromPlace.getY() - toPlace.getY());
+        int changeOfLocationY = fromPlace.getY() - toPlace.getY();
+
+        int startingYPositionOfWhitePawn = 1;
+        int startingYPostitionOfBlackPawn = 6;
 
         if (piece.getColor().equals(Color.WHITE)) {
-            if (moveType == MoveType.CAPTURE) {
+            if (moveType == MoveType.ATTACK) {
+                if (changeOfLocationX == 0 && changeOfLocationY == -1) {
+                    return true;
+                } else if (fromPlace.getY() == startingYPositionOfWhitePawn && changeOfLocationX == 0
+                        && (changeOfLocationY == -1 || changeOfLocationY == -2)) {
+                    return true;
+                }
+                return false;
+
+            } else if (moveType == MoveType.CAPTURE) {
                 if (changeOfLocationX == 1 && changeOfLocationY == -1) {
                     return true;
-
-                } else if (changeOfLocationX == 1 && changeOfLocationY == -1) {
-                    return true;
-                } else
-                    return false;
-
-            } else if (moveType.equals(MoveType.ATTACK)) {
-                if (fromPlace.getY() == 1 && changeOfLocationX == 0 && (changeOfLocationY == -1 || changeOfLocationY == -2)) {
-                    return true;
-
-                } else if (changeOfLocationX == 0 && changeOfLocationY == -1) {
-                    return true;
-                } else return true;
+                }
+                return false;
             }
-
         } else {
-            if (moveType == MoveType.CAPTURE) {
-                if (fromPlace.getY() == 6 && changeOfLocationX == 0 && changeOfLocationY == 1 || changeOfLocationY == 2) {
+            if (moveType == MoveType.ATTACK) {
+                if (changeOfLocationX == 0 && changeOfLocationY == 1) {
                     return true;
-                } else if (changeOfLocationX == 1 && changeOfLocationY == 0) {
+                } else if (fromPlace.getY() == startingYPostitionOfBlackPawn && changeOfLocationX == 0
+                        && (changeOfLocationY == 1 || changeOfLocationY == 2)) {
                     return true;
-                } else
-                    return false;
+                }
+                return false;
+
+            } else if (moveType == MoveType.CAPTURE) {
+                if (changeOfLocationX == 1 && changeOfLocationY == 1) {
+                    return true;
+                }
+                return false;
             }
         }
-
         return false;
     }
 }
+
+
+/*
+    @Override
+    public boolean validateMovePath(Coordinate fromPlace, Coordinate toPlace, Board board) throws InvalidMoveException {
+
+
+        if (fromPlace.getX()>toPlace.getX()&&fromPlace.getY()>toPlace.getY()) {
+            for (int x = fromPlace.getX() + 1, y = fromPlace.getY() + 1; x > toPlace.getX() && y > toPlace.getY(); x++, y++) {
+                Coordinate coordinate = new Coordinate(x, y);
+                if (board.getPieceAt(coordinate) != null) {
+                    throw new InvalidMoveException();
+                }
+            }
+        }
+        if (fromPlace.getX()>toPlace.getX()&&fromPlace.getY()<toPlace.getY()) {
+            for (int x = fromPlace.getX() + 1, y = fromPlace.getY() + 1; x > toPlace.getX() && y < toPlace.getY(); x++, y++) {
+                Coordinate coordinate = new Coordinate(x, y);
+                if (board.getPieceAt(coordinate) != null) {
+                    throw new InvalidMoveException();
+                }
+            }
+        }
+
+
+        if (fromPlace.getX()<toPlace.getX()&&fromPlace.getY()<toPlace.getY()){
+            for (int x=fromPlace.getX()+1,y=fromPlace.getY()+1;x<toPlace.getX()&&y<toPlace.getY();x++,y++) {
+                Coordinate coordinate = new Coordinate(x, y);
+                if (board.getPieceAt(coordinate) != null) {
+                    throw  new InvalidMoveException();
+                }
+            }
+
+        }
+        if (fromPlace.getX()<toPlace.getX()&&fromPlace.getY()>toPlace.getY()) {
+            for (int x = fromPlace.getX() + 1, y = fromPlace.getY() + 1; x < toPlace.getX() && y > toPlace.getY(); x++, y++) {
+                Coordinate coordinate = new Coordinate(x, y);
+                if (board.getPieceAt(coordinate) != null) {
+                    throw new InvalidMoveException();
+                }
+            }
+        }
+
+
+
+        return true;
+    }
+*/
 
